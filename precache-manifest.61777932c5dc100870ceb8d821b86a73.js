4c5ab6d264dd5d04db304ead724b8abf3ffbd8e5
Jubel <jubelcassioverridasilva@gmail.com>
2020-09-21 19:14:59 -0300

    Updates

---
+++ precache-manifest.61777932c5dc100870ceb8d821b86a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf8fb9aef5b7d221195c597a2c3a50d",
+    "url": "/server-side-tagging/index.html"
+  },
+  {
+    "revision": "192b8610101a83808210",
+    "url": "/server-side-tagging/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3241b1372d41eb815dea",
+    "url": "/server-side-tagging/static/js/2.cab62902.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/server-side-tagging/static/js/2.cab62902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192b8610101a83808210",
+    "url": "/server-side-tagging/static/js/main.2b7aa6b8.chunk.js"
+  },
+  {
+    "revision": "44b3247be3caf8e65bfc",
+    "url": "/server-side-tagging/static/js/runtime-main.724c55f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/server-side-tagging/static/media/logo.5d5d9eef.svg"
+  }
+]);
